2289376ac0a97c52d96e7b9d44e3737a41990add
Maciej Moroz <maciej.moroz89@gmail.com>
2019-09-24 23:25:36 +0200

    header - logo, tools - mob

--- src/js/app.js
+++ src/js/app.js
@@ -1,43 +1,43 @@
-const BODY = this.document.querySelector("body"),
-  btn_menu = this.document.querySelector("[data-menu-btn]");
+// const BODY = this.document.querySelector("body"),
+//   btn_menu = this.document.querySelector("[data-menu-btn]");
 
-//on scroll events
-window.onscroll = () => {
-  fixedMenu();
-};
+// //on scroll events
+// window.onscroll = () => {
+//   fixedMenu();
+// };
 
-// fixed menu
-let fixedMenu = () => {
-  if (window.pageYOffset > 100) {
-    nav.style.paddingTop = "20px";
-    nav.style.paddingBottom = "10px";
-    nav.style.height = "80px";
-    if (window.innerWidth > 1024) {
-      nav.style.height = "80px";
-    }
+// // fixed menu
+// let fixedMenu = () => {
+//   if (window.pageYOffset > 100) {
+//     nav.style.paddingTop = "20px";
+//     nav.style.paddingBottom = "10px";
+//     nav.style.height = "80px";
+//     if (window.innerWidth > 1024) {
+//       nav.style.height = "80px";
+//     }
 
-    nav.style.background = "rgba(112, 112, 112, 0.9)";
-  } else {
-    nav.style.paddingTop = "40px";
-    nav.style.background = "transparent";
-    nav.style.height = "100px";
-  }
-};
+//     nav.style.background = "rgba(112, 112, 112, 0.9)";
+//   } else {
+//     nav.style.paddingTop = "40px";
+//     nav.style.background = "transparent";
+//     nav.style.height = "100px";
+//   }
+// };
 
-// hamburger menu
-btn_menu.onclick = () => {
-  if (BODY.classList != "open-menu") {
-    BODY.classList.toggle("open-menu");
-  }
+// // hamburger menu
+// btn_menu.onclick = () => {
+//   if (BODY.classList != "open-menu") {
+//     BODY.classList.toggle("open-menu");
+//   }
 
-  if (nav.style.height === "100vh") {
-    nav.style.height = "70px";
-    if (window.pageYOffset < 100) {
-      nav.style.background = "transparent";
-      nav.style.height = "100px";
-    }
-  } else {
-    nav.style.background = "rgba(112, 112, 112, 0.9)";
-    nav.style.height = "100vh";
-  }
-};
+//   if (nav.style.height === "100vh") {
+//     nav.style.height = "70px";
+//     if (window.pageYOffset < 100) {
+//       nav.style.background = "transparent";
+//       nav.style.height = "100px";
+//     }
+//   } else {
+//     nav.style.background = "rgba(112, 112, 112, 0.9)";
+//     nav.style.height = "100vh";
+//   }
+// };
