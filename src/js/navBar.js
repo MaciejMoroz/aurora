2123bc9a254eac2326df60f64a192f3b579ad122
Maciej Moroz <maciej.moroz89@gmail.com>
2019-09-27 21:05:35 +0200

    nav menu desk, mob

---
+++ src/js/navBar.js
@@ -0,0 +1,24 @@
+const BODY = document.querySelector('body'),
+    hamburger = document.querySelector('[data-hamburger-btn]'),
+    coll = document.getElementsByClassName("colapse");
+
+hamburger.onclick = () => {
+    BODY.classList.toggle('open-navMenu');
+
+}
+
+
+
+
+
+for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        const content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}
