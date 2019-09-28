e5dc3ec39a7d709fb68400369ee8d04bc9ef3ee6
Maciej Moroz <maciej.moroz89@gmail.com>
2019-09-28 21:54:25 +0200

    nav menu changes

--- src/js/navBar.js
+++ src/js/navBar.js
@@ -1,8 +1,14 @@
 const BODY = document.querySelector('body'),
-    hamburger = document.querySelector('[data-hamburger-btn]');
-
+    hamburger = document.querySelector('[data-hamburger-btn]'),
+    checkboxes = document.querySelectorAll("input[type='checkbox']");
 
 hamburger.onclick = () => {
     BODY.classList.toggle('open-navMenu');
     hamburger.classList.toggle('active');
 }
+
+checkboxes.map((checkbox, i) => {
+    console.log(i)
+    console.log(checkbox)
+
+})
--- web/js/modules/navBar.js
+++ web/js/modules/navBar.js
@@ -1,9 +1,15 @@
 "use strict";
 
 var BODY = document.querySelector('body'),
-    hamburger = document.querySelector('[data-hamburger-btn]');
+    hamburger = document.querySelector('[data-hamburger-btn]'),
+    checkboxes = document.querySelectorAll("input[type='checkbox']");
 
 hamburger.onclick = function () {
   BODY.classList.toggle('open-navMenu');
   hamburger.classList.toggle('active');
 };
+
+checkboxes.map(function (checkbox, i) {
+  console.log(i);
+  console.log(checkbox);
+});
