f376bf6a12c0c8314569669de0100ff9fc1c03ad
Maciej Moroz <maciej.moroz89@gmail.com>
2019-09-29 11:17:19 +0200

    change html nav structure - checkbox nav, animation buttons fix on ie11

--- src/js/navBar.js
+++ src/js/navBar.js
@@ -1,14 +1,8 @@
 const BODY = document.querySelector('body'),
-    hamburger = document.querySelector('[data-hamburger-btn]'),
-    checkboxes = document.querySelectorAll("input[type='checkbox']");
+    hamburger = document.querySelector('[data-hamburger-btn]');
 
 hamburger.onclick = () => {
     BODY.classList.toggle('open-navMenu');
     hamburger.classList.toggle('active');
 }
 
-checkboxes.map((checkbox, i) => {
-    console.log(i)
-    console.log(checkbox)
-
-})
--- web/js/modules/navBar.js
+++ web/js/modules/navBar.js
@@ -1,15 +1,9 @@
 "use strict";
 
 var BODY = document.querySelector('body'),
-    hamburger = document.querySelector('[data-hamburger-btn]'),
-    checkboxes = document.querySelectorAll("input[type='checkbox']");
+    hamburger = document.querySelector('[data-hamburger-btn]');
 
 hamburger.onclick = function () {
   BODY.classList.toggle('open-navMenu');
   hamburger.classList.toggle('active');
 };
-
-checkboxes.map(function (checkbox, i) {
-  console.log(i);
-  console.log(checkbox);
-});
